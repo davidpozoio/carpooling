48bfcfb9f4fb016b5a3e9a7cffedc0a07957922d
davidpozoio <117756174+davidpozoio@users.noreply.github.com>
2023-11-17 10:44:41 -0500

    first commit

---
+++ carpooling/src/test/kotlin/com/acme/carpooling/CarpoolingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.acme.carpooling
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarpoolingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
